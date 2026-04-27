049aae50967d5a2d60d78dacfb91a59fbcb471e3
Wasit-karimi <wasitkarimi0@gmail.com>
2026-04-27 22:28:51 +0430

    add tailwind component add api data to details page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,21 +17,29 @@ const Home = () => {
       y: 0,
       opacity: 1,
       duration: 1,
-      ease: "power1.inOut"
+      ease: "power1.inOut",
     });
 
-    tl.to(".fade", {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power1.inOut",
-    }, "-=0.5");
+    tl.to(
+      ".fade",
+      {
+        opacity: 1,
+        duration: 1.2,
+        ease: "power1.inOut",
+      },
+      "-=0.5",
+    );
 
-    tl.to(".rise", {
-      y: 0,
-      opacity: 1,
-      duration: 1,
-      ease: "power1.inOut",
-    }, "<");
+    tl.to(
+      ".rise",
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      },
+      "<",
+    );
   }, []);
   return (
     <section className="w-full h-100 flex flex-col justify-center items-center">
@@ -43,8 +51,7 @@ const Home = () => {
       </p>
       <button
         onClick={() => navigate("/jobs")}
-        className="bg-linear-to-r from-white to-blue-400 flex gap-1
-         justify-center items-center px-2 py-1 rounded  cursor-pointer hover:shadow-md hover:shadow-blue-300 transition-all duration-300 rise"
+        className="btn-primary rise"
       >
         Explore Jobs <LuArrowRight />
       </button>
--- src/pages/JobDetails.tsx
+++ src/pages/JobDetails.tsx
@@ -11,9 +11,14 @@ const JobDetails = () => {
   return (
     <div>
       <h1 className="font-bold text-lg flex flex-col gap-5">{job.title}</h1>
-      <p>{job.descriptionBreakdown.oneSentnceJobSummary}</p>
-      <button onClick={() => navigate(`${job.url}`)}>Apply</button><br />
-      <button onClick={() => navigate("/jobs")}>Jobs</button>
+      <p>{job.descriptionBreakdown.oneSentenceJobSummary}</p>
+      <p><strong>Type : </strong>{job.descriptionBreakdown.employmentType}</p>
+      <p><strong>Work Model : </strong>{job.descriptionBreakdown.workModel}</p>
+      <p><strong>Salary : </strong> ${job.descriptionBreakdown.salaryRangeMinYearly} - ${job.descriptionBreakdown.salaryRangeMaxYearly} per year</p>
+      <p><strong>Location : </strong>{job.locationAddress}</p>
+      <button className="btn-primary"><a href={job.url} target="_blank">Apply</a></button>
+      <br />
+      <button onClick={() => navigate("/jobs")} className="btn-primary">Jobs</button>
     </div>
   );
 };
