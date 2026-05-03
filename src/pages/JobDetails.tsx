e14b2444ac304d2d4252a616c0fc33763ad52a4e
Wasit-karimi <wasitkarimi0@gmail.com>
2026-05-03 11:50:52 +0430

    optional chainig for description

--- src/pages/JobDetails.tsx
+++ src/pages/JobDetails.tsx
@@ -5,16 +5,23 @@ const JobDetails = () => {
   const navigate = useNavigate();
 
   const job = useLoaderData();
-  console.log(job); 
+  console.log(job);
 
   if (!job) return <NotFound />;
 
   return (
     <div className="w-full lg:w-[50%] md:w-[90%] shadow-md hover:shadow-lg hover:shadow-blue-300 transition-all duration-300 rounded-md cursor-pointer ">
       <div className="px-3 py-1 border border-gray-300 hover:border-blue-300 transition-all duration-300 rounded-md">
-        <h1 className="font-bold text-lg mx-auto text-center my-2">{job.title}</h1>
-        {/* <p className="text-justify">{job.descriptionBreakdown.oneSentenceJobSummary}</p> */}
-        {/* <p className="my-1">
+        <h1 className="font-bold text-lg mx-auto text-center my-2">
+          {job.title}
+        </h1>
+
+        {job.descriptionBreakdown.oneSentenceJobSummary && (
+          <p className="text-justify">
+            {job.descriptionBreakdown.oneSentenceJobSummary}
+          </p>
+        )}
+        <p className="my-1">
           <strong>Type : </strong>
           {job.descriptionBreakdown.employmentType}
         </p>
@@ -26,20 +33,21 @@ const JobDetails = () => {
           <strong>Salary : </strong> $
           {job.descriptionBreakdown.salaryRangeMinYearly} - $
           {job.descriptionBreakdown.salaryRangeMaxYearly} per year
-        </p> */}
+        </p>
         <p className="mb-1">
           <strong>Location : </strong>
           {job.locationAddress}
         </p>
         <div className="w-full flex justify-center gap-3 my-2">
-        <button className="btn-primary">
-          <a href={job.url} target="_blank">
-            Page
-          </a>
-        </button>
-        <button onClick={() => navigate("/jobs")} className="btn-primary">
-          Jobs
-        </button></div>
+          <button className="btn-primary">
+            <a href={job.url} target="_blank">
+              Page
+            </a>
+          </button>
+          <button onClick={() => navigate("/jobs")} className="btn-primary">
+            Jobs
+          </button>
+        </div>
       </div>
     </div>
   );
