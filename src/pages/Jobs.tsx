c83962da0ccc525da1a054f25835a5e01590606f
Wasit-karimi <wasitkarimi0@gmail.com>
2026-05-03 12:49:53 +0430

    regenerated types

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -4,27 +4,35 @@ import { Link } from "react-router-dom";
 import type { Job } from "../types/types";
 
 const Home = () => {
-  const { data } = useQuery(jobsQuery);
+  const { data, isLoading } = useQuery(jobsQuery);
+
+  
+  if (isLoading) return <div>Loading...</div>;
 
   const jobs = data?.map((job: Job) => (
-          <li className="hover:text-blue-600 hover:translate-x-1 transition-all duration-300 my-2 bg-linear-to-r to-blue-400 rounded-md p-2 w-full text-center">
-            <Link to={`/jobs/${job._id}`} key={job.title}>
-              {job.title}
-            </Link>
-          </li>
-        ))
+    <li
+      key={job.title}
+      className="hover:text-blue-600 hover:translate-x-1 transition-all duration-300 my-2 bg-linear-to-r to-blue-400 rounded-md p-2 w-full text-center"
+    >
+      <Link to={`/jobs/${job._id}`} key={job.title}>
+        {job.title}
+      </Link>
+    </li>
+  ));
+
 
   return (
     <section className="w-full">
       <div className="px-3 md:mx-auto md:w-[80%]">
+        <h1 className="text-center capitalize text-2xl my-4 font-bold shadow-md shadow-blue-300 rounded-md">
+          jobs
+        </h1>
 
-        <h1 className="text-center capitalize text-2xl my-4 font-bold shadow-md shadow-blue-300 rounded-md">jobs</h1>
+        <p className="my-4 border-b border-gray-400 pb-2">
+          Explore our list of jobs
+        </p>
 
-        <p className="my-4 border-b border-gray-400 pb-2">Explore our list of jobs</p>
-
-      <ul className="list-decimal my-5">
-        {jobs}
-      </ul>
+        <ul className="list-decimal my-5">{jobs}</ul>
       </div>
     </section>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,52 +1,33 @@
-export interface Job {
-  owner: Owner
-  locationAddress: string
-  title: string
-  type: string
-  url: string
-  createdAt: string
-  skills_suggest: string[]
-  descriptionBreakdown: DescriptionBreakdown
-  locationCoordinates: LocationCoordinates
-  department: string
-  seniority: string
-  updatedAt: string
-  _id: string
-}
 
-export interface Owner {
-  benefits: Benefits
-  funding: string
-  companyName: string
-  rating: string
-  evaluatedSize: number
-  photo: string
-  locationAddress: string
-  teamSize: number
-  _id: string
-  locationCoordinates: LocationCoordinates
-  isClaimed: boolean
-  sector: string
-  slug: string
-  activated: boolean
-}
+type Benefits = {
+  benefits: string[];
+  title: string;
+};
 
-export interface Benefits {
-  benefits: string[]
-  title: string
-}
+type Owner = {
+  benefits: Benefits;
+  funding: string;
+  companyName: string;
+  rating: string;
+  evaluatedSize: number | null;
+  photo: string;
+  locationAddress: string;
+  teamSize: number;
+  _id: string;
+  locationCoordinates: unknown | null;
+  isClaimed: boolean;
+  slug: string;
+  activated: boolean;
+};
 
-export interface DescriptionBreakdown {
-  oneSentenceJobSummary: string
-  keywords: string[]
-  employmentType: string
-  workModel: string
-  salaryRangeMinYearly: number
-  skillRequirements: string[]
-  salaryRangeMaxYearly: number
-}
-
-export interface LocationCoordinates {
-  lon: number
-  lat: number
-}
+export type Job = {
+  owner: Owner;
+  createdAt: string;
+  locationAddress: string;
+  locationCoordinates: unknown | null;
+  title: string;
+  type: string;
+  url: string;
+  updatedAt: string;
+  _id: string;
+};
